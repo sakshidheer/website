2e7b4703bfba77bf9cb3aa725ca33566ddb09146
sakshidheer <69763547+sakshidheer@users.noreply.github.com>
2021-12-11 01:49:12 +0530

    Margin in blog

--- src/components/BlogLayout.js
+++ src/components/BlogLayout.js
@@ -1,25 +1,29 @@
-import { blog, infosection, tag, info } from './BlogLayout.module.css'
+import { blog, infosection, tag, info, blogSection } from './BlogLayout.module.css'
 import Layout from './Layout';
 require("prismjs/themes/prism-tomorrow.css");
 
 function BlogLayout({ pageContext, children }) {
   let tags = pageContext.frontmatter.tags.split(',').map(item => {
-    return '#'+item.trim();
+    return '#' + item.trim();
   })
   return (
     <Layout cls={blog}>
-      <h1>{pageContext.frontmatter.title}</h1>
-      <div className={infosection}>
-        <div className={tag}>
-        {tags.join(' ')}
+      <div className={blogSection}>
+
+        <h1>{pageContext.frontmatter.title}</h1>
+        <div className={infosection}>
+          <div className={tag}>
+            {tags.join(' ')}
+          </div>
+          <div className={info}>
+            <span><b>By </b> {pageContext.frontmatter.author}</span>
+            <span><b>Posted on </b> {pageContext.frontmatter.date}</span>
+          </div>
+
         </div>
-        <div className={info}>
-        <span><b>By </b> {pageContext.frontmatter.author}</span>
-        <span><b>Posted on </b> {pageContext.frontmatter.date}</span>
-        </div>
-        
+
+        {children}
       </div>
-      {children}
     </Layout>
   );
 }
