e91227b37304d84af88869e067d383ffdca49868
sakshidheer <69763547+sakshidheer@users.noreply.github.com>
2021-12-22 00:51:49 +0530

    Add twitter button

--- src/components/BlogLayout.js
+++ src/components/BlogLayout.js
@@ -1,4 +1,6 @@
+import { Helmet } from 'react-helmet';
 import { blog, infosection, tag, info, blogSection } from './BlogLayout.module.css'
+import IconLink from './IconLink';
 import Layout from './Layout';
 require("prismjs/themes/prism-tomorrow.css");
 
@@ -7,20 +9,29 @@ function BlogLayout({ pageContext, children }) {
     return '#' + item.trim();
   })
   return (
-    <Layout cls={blog}>
-      <div className={blogSection}>
+    <>
+      <Helmet>
+        <title>Sakshi Dheer</title>
+        <link href="https://use.fontawesome.com/releases/v5.0.1/css/all.css" rel="stylesheet" ></link>
+      </Helmet>
+      <Layout cls={blog}>
+        <div className={blogSection}>
 
-        <h1>{pageContext.frontmatter.title}</h1>
-        <div className={infosection}>
-          <div className={tag}>
-            {tags.join(' ')}
+          <h1>{pageContext.frontmatter.title}</h1>
+          <div className={infosection}>
+            <div className={tag}>
+              {tags.join(' ')}
+
+            </div>
+            <IconLink link={'https://twitter.com/intent/tweet?text=' + 
+            pageContext.frontmatter.title + ' ' + location.href+'&hashtags='+pageContext.frontmatter.tags}
+              icon="fab fa-twitter" iconColor="#00acee" />
           </div>
 
+          {children}
         </div>
-
-        {children}
-      </div>
-    </Layout>
+      </Layout>
+    </>
   );
 }
 
